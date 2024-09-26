98e5f19c2a8e5243b9b08c8b5b90122a53dd8c4d
Majd833 <lina.azzam.1611@gmail.com>
2024-09-26 18:42:59 +0300

    Reverse a string

---
+++ Reverse a string.py
@@ -0,0 +1,6 @@
+string=str(input("Enter your own string:"))
+string2=("")
+for i in string:
+    string2= i + string2
+print("\nThe original string is:", string)   
+print("\n the reversed string is:", string2) 
